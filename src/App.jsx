4ead9f41c334f908fcb9b7eda8e7e6c9f6032252
HectorRamirez <eduardo.846@gmail.com>
2023-01-11 18:26:11 -0400

    feat: add card service

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Button } from "react-bootstrap";
 import Introduction from "./components/Introduccion";
 import NavbarMenu from "./components/NavbarMenu"
+import Services from "./components/Services";
 
 function App() {
   return (
     <div>
       <NavbarMenu />
       <Introduction/>
+      <Services />
     </div>
   );
 }
